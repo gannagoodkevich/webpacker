f552678523d7ee54ec38aa8983e0468cb8b7d439
gannagoodkevich <gannagoodkevich@gmail.com>
2020-02-25 13:48:36 +0300

    Configure packs and layouts for Admin and User

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,37 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '' };
+
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleSubmitName = this.handleSubmitName.bind(this);
+    }
+
+    handleChangeName(event) {
+        this.setState({name: event.target.value});
+    }
+
+    handleSubmitName(event) {
+        localStorage.setItem("name", this.state.name);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="form-group">
+                <form onSubmit={this.handleSubmitName}>
+                    <label>
+                        Enter name
+                        <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+                    </label>
+                    <input type="submit" value="Отправить" className="btn-primary"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld
